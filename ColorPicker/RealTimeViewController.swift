72d935d426bc41cec3f8353978a30f697e5b894c
LinShiwei <477623614@qq.com>
2016-12-09 12:15:53 +0800

    [Coding] Add GPUImage

---
+++ ColorPicker/RealTimeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RealTimeViewController.swift
+//  ColorPicker
+//
+//  Created by Linsw on 16/12/9.
+//  Copyright © 2016年 Linsw. All rights reserved.
+//
+
+import UIKit
+import GPUImage
+
+class RealTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
