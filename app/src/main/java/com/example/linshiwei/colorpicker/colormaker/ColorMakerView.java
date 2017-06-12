31d7ed9193f3af315551e6797463c4262fb4a1fe
LinShiwei <477623614@qq.com>
2017-06-12 17:33:47 +0800

    [Coding] add colormakerview layout xml

--- app/src/main/java/com/example/linshiwei/colorpicker/colormaker/ColorMakerView.java
+++ app/src/main/java/com/example/linshiwei/colorpicker/colormaker/ColorMakerView.java
@@ -3,14 +3,22 @@ package com.example.linshiwei.colorpicker.colormaker;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 
+import com.example.linshiwei.colorpicker.R;
+
 public class ColorMakerView extends LinearLayout {
+
+    private RGBMakerView mRGBMakerView;
+    private HEXMakerView mHEXMakerView;
 
     private MakerInputMode inputMode;
 
     public ColorMakerView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.color_maker_view,this,true);
     }
 
     public MakerInputMode getInputMode(){
