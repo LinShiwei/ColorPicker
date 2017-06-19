95f5340fad00e3f44c5593b3091241cab7be4a73
LinShiwei <477623614@qq.com>
2017-06-19 15:53:36 +0800

    [Coding] color information view

--- app/src/main/java/com/example/linshiwei/colorpicker/mainview/ColorInformationView.java
+++ app/src/main/java/com/example/linshiwei/colorpicker/mainview/ColorInformationView.java
@@ -1,6 +1,7 @@
 package com.example.linshiwei.colorpicker.mainview;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -17,7 +18,10 @@ import static com.example.linshiwei.colorpicker.addcolor.MakerInputMode.dec;
 
 public class ColorInformationView extends LinearLayout {
 
+    private int mCurrentColor = Color.WHITE;
 
+    private View mColorIndicationView;
+    private ColorComponentsContainerView mColorComponentsContainerView;
 
     public ColorInformationView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -25,7 +29,22 @@ public class ColorInformationView extends LinearLayout {
         LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View v = inflater.inflate(R.layout.color_information_view,this,true);
 
+        mColorComponentsContainerView = (ColorComponentsContainerView) v.findViewById(R.id.color_comp_container);
+        mColorIndicationView = v.findViewById(R.id.color_preview_view);
+
+        //待完善
+
 
     }
 
+    public void setCurrentColor(int currentColor) {
+        mCurrentColor = currentColor;
+        mColorIndicationView.setBackgroundColor(mCurrentColor);
+        mColorComponentsContainerView.color = mCurrentColor;
+        mColorComponentsContainerView.updateColorValue();
+    }
+
+    public int getCurrentColor() {
+        return mCurrentColor;
+    }
 }
